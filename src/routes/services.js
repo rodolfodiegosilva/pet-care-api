aa15f443356327d85b7cd9c4d88ae35368e5a387
Samuel Barbosa <j.samuel.s.b@gmail.com>
2024-10-15 17:17:10 -0300

    Fix Appointment Modules

--- src/routes/services.js
+++ src/routes/services.js
@@ -10,8 +10,8 @@ router.get('/', serviceController.getAllServices);
 router.get('/:id', serviceController.getServiceById);
 
 // Rotas protegidas e restritas a certos papéis
-router.post('/', authMiddleware, authorize(['admin', 'manager']), serviceController.createService);
-router.patch('/:id', authMiddleware, authorize(['admin', 'manager']), serviceController.updateService);
-router.delete('/:id', authMiddleware, authorize(['admin', 'manager']), serviceController.deleteService);
+router.post('/', authMiddleware, authorize(['supervisor', 'manager']), serviceController.createService);
+router.patch('/:id', authMiddleware, authorize(['supervisor', 'manager']), serviceController.updateService);
+router.delete('/:id', authMiddleware, authorize(['supervisor', 'manager']), serviceController.deleteService);
 
 module.exports = router;
